66ca1c4e4b33ede4d45dcd2e1f61e80b94d19d5f
윤석 최 <ckasn852@gmail.com>
2025-07-01 09:56:41 +0900

    카드 클래스에 함수 포인터 추가
    `Card` 클래스에 `executeCard_`라는 함수 포인터 변수를 추가했습니다.
    기본 생성자에서 `nullptr`로 초기화되며, 새로운 생성자는 `executeCard` 매개변수를 통해 해당 포인터를 초기화합니다.
    `getPowerPoint` 메서드는 변경되지 않았고, `executeCard` 메서드는 선언만 되어 있습니다.

--- Game1/Card.h
+++ Game1/Card.h
@@ -6,12 +6,13 @@ class Card
 private:
 	string name;
 	int powerPoint_;
+	void (*executeCard_)(void);
 
 public:
-	Card(int powerPoint) : powerPoint_(powerPoint) {}
+	Card(int powerPoint) : powerPoint_(powerPoint), executeCard_(nullptr) {}
+
+	Card(int powerPoint, void (*executeCard)(void)) :powerPoint_(powerPoint), executeCard_(executeCard) {}
 
 	int getPowerPoint() const { return powerPoint_; }
-
-	virtual void executeCard();
 };
 
