da6c4c721d96a167cb1703bff3e3fafdc2c4927f
sarah <sarahhan.one@gmail.com>
2025-07-02 17:42:44 +0900

    add boundary check

--- Game1/Pokemon.cpp
+++ Game1/Pokemon.cpp
@@ -40,15 +40,15 @@ vector<Position> Pokemon::excuteCard(vector<vector<Pokemon*>>& map, int cardInd)
 vector<Position> Pokemon::rangeMap(vector<vector<Pokemon*>>& map, vector<int>& range) {
 	vector<Position> tmp;
 	for (auto& point : range) {
-		if (point == 1) tmp.push_back({ pos_.y - 1, pos_.x - 1 });
-		if (point == 2) tmp.push_back({ pos_.y, pos_.x - 1 });
-		if (point == 3) tmp.push_back({ pos_.y + 1, pos_.x - 1 });
-		if (point == 4) tmp.push_back({ pos_.y - 1, pos_.x });
-		if (point == 5) tmp.push_back({ pos_.y, pos_.x });
-		if (point == 6) tmp.push_back({ pos_.y + 1, pos_.x });
-		if (point == 7) tmp.push_back({ pos_.y - 1, pos_.x + 1 });
-		if (point == 8) tmp.push_back({ pos_.y, pos_.x + 1 });
-		if (point == 9) tmp.push_back({ pos_.y + 1, pos_.x + 1 });
+		if (point == 1 && pos_.y > 0 && pos_.x - 1)                  tmp.push_back({ pos_.y - 1, pos_.x - 1 });
+		if (point == 2 && pos_.x > 0)                                tmp.push_back({ pos_.y, pos_.x - 1 });
+		if (point == 3 && pos_.y + 1 < MAX_Y && pos_.x > 0)          tmp.push_back({ pos_.y + 1, pos_.x - 1 });
+		if (point == 4 && pos_.y > 0)                                tmp.push_back({ pos_.y - 1, pos_.x });
+		if (point == 5)                                              tmp.push_back({ pos_.y, pos_.x });
+		if (point == 6 && pos_.y + 1 < MAX_Y)                        tmp.push_back({ pos_.y + 1, pos_.x });
+		if (point == 7 && pos_.y > 0 && pos_.x + 1 < MAX_X)          tmp.push_back({ pos_.y - 1, pos_.x + 1 });
+		if (point == 8 && pos_.x + 1 < MAX_X)                        tmp.push_back({ pos_.y, pos_.x + 1 });
+		if (point == 9 && pos_.y + 1 < MAX_Y && pos_.x + 1 < MAX_X)  tmp.push_back({ pos_.y + 1, pos_.x + 1 });
 	}
 	return tmp;
 }
--- Game1/Pokemon/Pikachu.cpp
+++ Game1/Pokemon/Pikachu.cpp
@@ -8,11 +8,15 @@ vector<Position> Pikachu::thunderbolt(vector<vector<Pokemon*>>& map)
     vector<Position> rangeAttack = rangeMap(map, range);
 
     for (auto& attackPos : rangeAttack) {
-        Pokemon* target = map[attackPos.y][attackPos.x];
-        if (target != nullptr) {
-            target->takeDamage(damage);
+        if (attackPos.x == pos_.x && attackPos.y == pos_.y) { // at current position
+            continue;
+        }
+        else {
+            Pokemon* target = map[attackPos.y][attackPos.x];
+            if (target != nullptr) {
+                target->takeDamage(damage);
+            }
         }
     }
-
     return rangeAttack;
 }
